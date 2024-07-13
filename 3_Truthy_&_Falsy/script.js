5aa73326139ebd94b7f9d50f7c42ac1b1cbc1faf
Its-2Sharp <96673478+Its-2Sharp@users.noreply.github.com>
2024-07-14 01:10:38 +0530

    Day 1
    Saturday midnight merge

---
+++ 3_Truthy_&_Falsy/script.js
@@ -0,0 +1,14 @@
+console.log('truthy and falsy');
+let myName;
+console.log('myName:',myName);
+
+
+if(myName){
+    console.log('myName:',myName);
+}else{
+    console.log('my name is not defined yet');
+}
+if (1<=2) {
+    myName = 'Tushar';
+    console.log('myName:',myName);
+   }
